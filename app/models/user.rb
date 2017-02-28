4af7ff74c4abae908173b22b5ed85a2af12a2c98
Elif Sezgin <elifmsezgin@gmail.com>
2017-02-28 14:02:15 -0800

    Created user modal and authentication

---
+++ app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  image_url       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates :first_name, :last_name, :image_url, :email, :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token!
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by_username(username)
+    if user && user.is_password?(password)
+      return user
+    end
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token!
+    self.session_token ||= generate_session_token
+  end
+end
